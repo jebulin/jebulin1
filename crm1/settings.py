e27d0a99a2a5feb621d718b65fe5a9eba05a569e
jebulin <61864666+jebulin@users.noreply.github.com>
2020-08-23 23:05:53 +0530

    Add files via upload

--- crm1/settings.py
+++ crm1/settings.py
@@ -120,7 +120,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
