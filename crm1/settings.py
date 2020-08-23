884629361f8aff6fdaaeeda81b190959d9eada7e
Jebulin <jebulin3@gmail.com>
2020-08-23 14:12:47 +0530

    message

--- crm1/settings.py
+++ crm1/settings.py
@@ -121,7 +121,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
